cf3e9b310facece6f2a22e0a0a0b28042a2f5eab
trsekhar123 <36709859+trsekhar123@users.noreply.github.com>
2019-01-03 20:13:16 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,27 @@
+
+# coding: utf-8
+
+# In[3]:
+
+
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name='EmailBodyRetriever',
+    version='0.1dev',
+    packages=['email_body_retriever',],
+    author='Rajasekhar Thiruthuvaraj',
+    author_email='trsekhar.123@gmail.com',
+    url='https://github.com/trsekhar123/EmailBodyRetriever',
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.md').read(),
+)
+
